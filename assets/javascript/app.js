c0201aada26e6b8062d017852e2ca982e4768897
Andrew Carr <andrewpcarr@gmail.com>
2017-03-04 01:05:15 -0600

    added still images and click function that's not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,31 @@
+// Array for the giphy buttons
 var animals = ['horse', 'cat', 'dog', 'chicken', 'duck', 'donkey', 'goat', 'cow', 'turkey', 'goose', 'rabbit'];
 
+// Create the initial buttons
 for (var i = 0; i < animals.length; i++) {
 	console.log(animals[i]);
-	
 	animalBtn = $("<button>");
-        
     animalBtn.addClass("animal");
-
     animalBtn.attr("data-animal", animals[i]);
-
     animalBtn.text(animals[i]);
       
     $(".buttons").append(animalBtn);
+}
 
-
-    }
-
-
-
+// AJAX call to GiphyAPI and addings images to DOM
 $(".animal").on("click", function() {
-      var animal = $(this).attr("data-animal");
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-        animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        .done(function(response) {
+	$('.gifs').empty();
+
+    var animal = $(this).attr("data-animal");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+    	animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .done(function(response) {
           var results = response.data;
 
           for (var i = 0; i < results.length; i++) {
@@ -38,7 +36,11 @@ $(".animal").on("click", function() {
             var p = $("<p>").text("Rating: " + rating);
 
             var animalImage = $("<img>");
-            animalImage.attr("src", results[i].images.fixed_height.url);
+            animalImage.attr("src", results[i].images.original_still.url);
+            animalImage.attr('data-still', results[i].images.original_still.url);
+            animalImage.attr('data-animate', results[i].images.fixed_height.url);
+            animalImage.attr('data-state', 'still');
+            animalImage.attr('class', 'gif');
 
             gifDiv.prepend(p);
             gifDiv.prepend(animalImage);
@@ -47,3 +49,20 @@ $(".animal").on("click", function() {
           }
         });
     });
+
+// Click event to start and pause the gif
+$(".gif").on("click", function() {
+
+    var state = $(this).attr('data-state');
+    console.log(state);
+    if (state === 'still') {
+        var dataAnimate = $(this).attr('data-animate');
+        $(this).attr('src', dataAnimate);
+        $(this).attr('data-state', 'animate');
+      } else {
+        var dataStill = $(this).attr('data-still');
+        $(this).attr('src', dataStill);
+        $(this).attr('data-state', 'still');
+      }
+
+});
