3ceef2908df16df03cde83cb73760e4e3724d459
Andrew Carr <andrewpcarr@gmail.com>
2017-03-04 23:12:12 -0600

    switched API to https://

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,7 +39,7 @@ $(".buttons").on("click", '.animal', function() {
 	$('.gifs').empty();
 
     var animal = $(this).attr("data-animal");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     	animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
     $.ajax({
